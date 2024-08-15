0598a6d7d25a4c89ee05a7d74534586f77392f18
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-08-15 22:42:33 +0700

    everything

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'navbrown': '#5F4628',
+      },
+      fontFamily: {
+        'Arimo': ["Arimo"],
+        'Fredoka': ["Fredoka"]
+      }
+    },
+  },
+  plugins: [],
+}
+
