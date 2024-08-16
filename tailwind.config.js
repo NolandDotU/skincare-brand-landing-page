46b6c2860641834e1035de18b836dd7509e35441
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-08-16 09:32:36 +0700

    part 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'navbrown': '#5F4628',
+        primary: "#97A97C",
+        myGreen: "#718355",
+        myBrown: "#524632",
+        myLime: "#E9F5DB",
       },
-      fontFamily: {
-        'Arimo': ["Arimo"],
-        'Fredoka': ["Fredoka"]
-      }
+      backgroundImage: {
+        "login-img":
+          "url('https://s3-alpha-sig.figma.com/img/c51e/9043/f3797927c60b3248c3723f14526075a3?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Tlvsfyws-A7SLQYj6Wrqbd0N~yGXaj8W1fP2wtHEFzBlXpcRVHwYER7EItV3DclHo715KmGv8yyIdUA-wAHaGBQl~KsF44vUsM0IUjsrQqRgZBdIi7ofsod85taDpwhjn-qLIe2Rr3BWbKIVgF5GPMoEms5ojrvrF-dMPPFeweUg2S~J0do4utELH29~rH18cCXCTdtFd6bMryNG7EwFqKCIhnpE4sYcpF0h6-UHuzhNIgpNSTjfC980dVvNqY1PJOgXvbRlxT3h~kfGhd6BE62IAsku~qiVVp7B0-P6VKlxrTBLYCmLWkVjrQYz1LG3jCXr~b0HhC9qaQpWfj3T4Q__')",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [],
+  },
+};
